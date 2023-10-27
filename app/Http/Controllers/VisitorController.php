a2715f3ee955502a68a36192a8c17e4b28c2f7b1
wesleysnt <103561039+wesleysnt@users.noreply.github.com>
2023-10-27 19:10:25 +0700

    tes

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -6,6 +6,9 @@ use App\Models\Foto;
 use App\Models\Kabupaten;
 use App\Models\ObjekWisata;
 use App\Models\SubKategori;
+use Gtmassey\LaravelAnalytics\Analytics;
+use Gtmassey\LaravelAnalytics\Request\Metrics;
+use Gtmassey\Period\Period;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 
@@ -18,7 +21,18 @@ class VisitorController extends Controller
      */
     public function index()
     {
-        return view('index');
+
+        $report = Analytics::query()
+            ->setMetrics(
+                fn (Metrics $metrics) => $metrics
+                    ->active1DayUsers()
+                    ->active7DayUsers()
+                    ->active28DayUsers()
+            )
+            ->forPeriod(Period::defaultPeriod())
+            ->run();
+
+        return view('index', compact('report'));
     }
 
     public function benda()
@@ -29,13 +43,13 @@ class VisitorController extends Controller
         $kabupaten = Kabupaten::all();
         $sub_kategori = SubKategori::all();
 
-        return view('visitor.benda', compact(['data','kabupaten','sub_kategori']));
+        return view('visitor.benda', compact(['data', 'kabupaten', 'sub_kategori']));
     }
 
     public function takBenda()
     {
         $data = ObjekWisata::with(['fotos', 'subKategori:id,nama,kategori_id', 'subKategori.kategori:id,nama', 'kabupaten:id,nama'])
-            ->where('status','Belum')
+            ->where('status', 'Belum')
             ->get();
 
         return view('visitor.takBenda', compact('data'));
